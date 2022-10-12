513f874de3b39346dc50d8c0e465b9ff3fb527f3
Urumasi <urumasi@email.cz>
2022-10-12 15:36:13 +0000

    Remove clutter from manage script

--- manage.py
+++ manage.py
@@ -6,7 +6,6 @@ from flask_script import (
     Server,
     Shell,
     Manager,
-    prompt_bool,
 )
 
 def _make_context():
@@ -23,20 +22,6 @@ manager = Manager(app)
 manager.add_command('runserver', Server(host='0.0.0.0'))
 manager.add_command('shell', Shell(make_context=_make_context))
 manager.add_command('db', MigrateCommand)
-
-
-@manager.command
-def drop_db():
-    """Drops data tables."""
-    if prompt_bool('Are you sure?'):
-        db.drop_all()
-
-
-@manager.command
-def recreate_db():
-    """Same as running drop_db() and create_db()."""
-    drop_db()
-    create_db()
 
 
 if __name__ == '__main__':
