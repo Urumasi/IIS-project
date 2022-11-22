8a4fd0d9539d1cd49baeb96dcd16b6c8def8d0fd
Urumasi <psalaba@redhat.com>
2022-11-22 16:29:03 +0100

    Make test login only

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,5 @@
 from flask import current_app, render_template
+from flask_login import login_required, logout_user, current_user
 
 from . import auth
 from app.extensions import lm
@@ -8,7 +9,7 @@ from app.data import User
 def load_user(id):
     return User.get_by_id(int(id))
 
-
 @auth.route('/test')
+@login_required
 def index():
     return render_template("test.html")
