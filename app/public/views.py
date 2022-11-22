d003795fc1ffefedbec9ad123fd0ebee35a120ac
Urumasi <psalaba@redhat.com>
2022-11-22 12:21:14 +0100

    Fix views, WIP DB helpers

--- app/public/views.py
+++ app/public/views.py
@@ -1,18 +1,7 @@
-from flask import render_template
-
-from . import public
-from app.extensions import lm
-from app.data import User
-
-@lm.user_loader
-def load_user(id):
-        return User.get_by_id(int(id))
-
-from flask import current_app, render_template, flash, redirect, request, url_for
+from flask import current_app, render_template
 from flask_login import login_user
 
 from . import public
-# from app.public.forms import LoginForm, RegisterUserForm
 from app.extensions import lm
 from app.data import User
 
