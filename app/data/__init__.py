4bba2916079a7f663e96ce1ba6000e07047438ce
Urumasi <psalaba@redhat.com>
2022-11-24 12:29:16 +0100

    Fix automatic merge again

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -5,4 +5,3 @@ from app.data.models.user import User
 from app.data.models.terms import Term
 from app.data.models.news import News
 from app.data.models.course import Course
-from app.data.models.helper_tables import course_lecturers, course_students
