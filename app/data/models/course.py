26e337373c064a9f44cb0a5c6f758c49c0e202db
Urumasi <psalaba@redhat.com>
2022-11-24 12:31:48 +0100

    Fix automatic merge removing some tables

--- app/data/models/course.py
+++ app/data/models/course.py
@@ -2,6 +2,16 @@ from app.data import db
 from app.data.mixins import CRUDMixin
 from sqlalchemy import func
 
+course_students = db.Table('course_students',
+                           db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                           db.Column('course_id', db.Integer, db.ForeignKey('course.id'), primary_key=True)
+                           )
+
+course_lecturers = db.Table('course_lecturers',
+                            db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                            db.Column('course_id', db.Integer, db.ForeignKey('course.id'), primary_key=True)
+                            )
+
 
 class Course(db.Model, CRUDMixin):
     abbreviation = db.Column(db.String(length=30), nullable=False, unique=True)
