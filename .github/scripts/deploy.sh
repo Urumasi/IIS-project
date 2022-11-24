e8ed9360998cfca4c05d000136a4427bd345a708
Urumasi <psalaba@redhat.com>
2022-11-24 13:44:32 +0100

    Change permissions for private key

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,6 +2,7 @@ eval $(ssh-agent -s)
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 echo "$1" | tr -d '\r' > ~/.ssh/action_key
+chmod 700 ~/.ssh/action_key
 ssh-keyscan -p2200 urumasi.xyz >> ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
 ssh -i ~/.ssh/action_key -p2200 iis@urumasi.xyz "cd /var/www/iis.urumasi.xyz/ && git stash && git checkout master && git pull origin master && git stash pop"
