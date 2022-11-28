73e1ec32e72012a1518da7e6412dc02f951c6f9a
denger-spaghett <adamkulla@gmail.com>
2022-11-28 23:25:07 +0100

    minor adjustments

--- app/auth/forms/cr_news.py
+++ app/auth/forms/cr_news.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 import datetime
-from wtforms import StringField, RadioField, IntegerField, DateField
+from wtforms import StringField 
 from wtforms.validators import DataRequired, Length, NumberRange
-
+from wtforms.widgets import TextArea
 
 class CreateNewsForm(FlaskForm):
-    da_newz = StringField('Text', validators=[DataRequired("This field is required")])
+    da_newz = StringField('Text', validators=[DataRequired("This field is required")], widget=TextArea())
 
 
     def __init__(self, *args, **kwargs):
