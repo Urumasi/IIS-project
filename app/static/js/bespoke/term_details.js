f90c05361e428b19c90d31ae15933735efe7bd6b
Urumasi <psalaba@redhat.com>
2022-11-28 20:01:20 +0100

    Add spinner on click

--- app/static/js/bespoke/term_details.js
+++ app/static/js/bespoke/term_details.js
@@ -1,3 +1,17 @@
+const APPLY_LOADING = '<i class="fa fa-spinner"></i> Apply';
+const APPLY_SUCCESS = '<i class="fa fa-check-circle"></i> Apply';
+const APPLY_FAIL = '<i class="fa fa-check-times"></i> Apply';
+
 function apply_points(self) {
-    console.log(self);
+    let input = self.parentNode.parentNode.children[1].children[0];
+    if (input.value === '') {
+        return;
+    }
+    if (self.busy) {
+        return;
+    }
+    console.log("Click");
+    self.busy = true;
+    self.innerHTML = APPLY_LOADING;
+
 }
