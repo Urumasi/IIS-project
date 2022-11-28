59adb2cf0bcbf37e84d94def0afbe8a0bafe13cd
Urumasi <psalaba@redhat.com>
2022-11-28 20:44:37 +0100

    Add some debug prints

--- app/static/js/bespoke/term_details.js
+++ app/static/js/bespoke/term_details.js
@@ -3,16 +3,19 @@ const APPLY_SUCCESS = '<i class="fa fa-check-circle"></i> Apply';
 const APPLY_FAIL = '<i class="fa fa-check-times"></i> Apply';
 
 function send_point_update(button, user_id, term, points) {
+    console.log(button, user_id, term, points);
     button.innerHTML = APPLY_LOADING;
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             let result = JSON.parse(this.responseText);
+            console.log(result);
             if(result.status == "success") {
                 button.innerHTML = APPLY_SUCCESS;
             } else {
                 button.innerHTML = APPLY_FAIL;
             }
+            button.busy = false;
         }
     }
     xhttp.open("POST", "/api/change_points", true);
@@ -27,10 +30,13 @@ function send_point_update(button, user_id, term, points) {
 
 function apply_points(self, user_id, term) {
     let input = self.parentNode.parentNode.children[1].children[0];
+    console.log(self);
     if (input.value === '') {
+        console.log('Empty');
         return;
     }
     if (self.busy !== true) {
+        console.log('Busy');
         return;
     }
     self.busy = true;
