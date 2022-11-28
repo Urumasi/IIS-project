793fadf3b5912869aea32466a1148368a40e15bc
Urumasi <psalaba@redhat.com>
2022-11-28 19:21:32 +0100

    Hopefully adds study requests

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -7,3 +7,4 @@ from app.data.models.news import News
 from app.data.models.course import Course, course_lecturers, course_students
 from app.data.models.body import TermBody
 from app.data.models.course_requests import CourseRequest
+from app.data.models.study_requests import StudyRequest
